0cf492cca5a602034747cf15c392c0c2e6b15ffb
Benjamin Cohen <bac1087@gmail.com>
2016-01-20 21:35:08 -0500

    finished about_enums

--- about_enums.exs
+++ about_enums.exs
@@ -114,180 +114,180 @@ defmodule About_Enums do
 
   think "chunking can happen in steps" do
     list = [1, 2, 3, 4, 5, 6]
-    assert Enum.chunk(list, 2, 1) == __?
+    assert Enum.chunk(list, 2, 1) == [[1,2],[2,3],[3,4],[4,5],[5,6]]
   end
 
   think "chunking can have padding" do
     list = [1, 2, 3, 4, 5, 6]
-    assert Enum.chunk(list, 3, 2, [7]) == __?
+    assert Enum.chunk(list, 3, 2, [7]) == [[1,2,3],[3,4,5],[5,6,7]]
   end
 
   think "chunking by unique values" do
     list = [3, 4, 5, 6, 7, 8]
-    assert Enum.chunk_by(list, fn (x) -> x > 5 end) == __?
+    assert Enum.chunk_by(list, fn (x) -> x > 5 end) == [[3,4,5],[6,7,8]]
   end
 
   think "dropping items" do
     list = [1, 2, 3, 4]
-    assert Enum.drop(list, 2) == __?
+    assert Enum.drop(list, 2) == [3,4]
   end
 
   think "dropping a lot of items" do
     list = [1, 2, 3, 4]
-    assert Enum.drop(list, 10) == __?
+    assert Enum.drop(list, 10) == []
   end
 
   think "dropping with negative numbers" do
     list = [1, 2, 3, 4]
-    assert Enum.drop(list, -1) == __?
+    assert Enum.drop(list, -1) == [1,2,3]
   end
 
   think "dropping while a condition is met" do
     list = [1, 2, 3, 4]
-    assert Enum.drop_while(list, fn (x) -> x < 2 end) == __?
+    assert Enum.drop_while(list, fn (x) -> x < 2 end) == [2,3,4]
   end
 
   think "filtering" do
     list = [1, 2, 3, 4]
-    assert Enum.filter(list, fn (x) -> rem(x, 2) == 1 end) == __?
+    assert Enum.filter(list, fn (x) -> rem(x, 2) == 1 end) == [1,3]
   end
 
   think "filtering + mapping" do
     list = [1, 2, 3, 4]
-    assert Enum.filter_map(list, fn (x) -> rem(x, 2) == 1 end, &(&1 * 2)) == __?
+    assert Enum.filter_map(list, fn (x) -> rem(x, 2) == 1 end, &(&1 * 2)) == [2,6]
   end
 
   think "flat mapping" do
     list = Enum.flat_map([{1, 3}, {4, 6}], fn({x, y}) -> x..y end)
-    assert list == __?
+    assert list == [1,2,3,4,5,6]
   end
 
   think "joining" do
     list = [1, 2, 3]
-    assert Enum.join(list) == __?
+    assert Enum.join(list) == "123"
   end
 
   think "joining with a separator" do
     list = [1, 2, 3]
-    assert Enum.join(list, ",") == __?
+    assert Enum.join(list, ",") == "1,2,3"
   end
 
   think "mapping and joining" do
     list = [1, 2, 3]
-    assert Enum.map_join(list, fn (x) -> x * 2 end) == __?
+    assert Enum.map_join(list, fn (x) -> x * 2 end) == "246"
   end
 
   think "map reduce" do
     list = [4, 5, 6]
-    assert Enum.map_reduce(list, 0, fn (x, acc) -> {x * 2, x + acc} end) == __?
+    assert Enum.map_reduce(list, 0, fn (x, acc) -> {x * 2, x + acc} end) == {[8,10,12], 15}
   end
 
   think "zipping" do
     list_1 = [1, 2, 3]
     list_2 = [4, 5, 6]
-    assert Enum.zip(list_1, list_2) == __?
+    assert Enum.zip(list_1, list_2) == [{1,4}, {2,5}, {3,6}]
   end
 
   think "what's the maximum value in this list?" do
     list = [6, 1, 5, 2, 4, 3]
-    assert Enum.max(list) == __?
+    assert Enum.max(list) == 6
   end
 
   think "getting the maximum by function" do
     list = ["the", "longest", "word", "is", "expected"]
-    assert Enum.max_by(list, &String.length(&1)) == __?
+    assert Enum.max_by(list, &String.length(&1)) == "expected"
   end
 
   think "what's the minimum value in this list?" do
     list = [6, 1, 5, 2, 4, 3]
-    assert Enum.min(list) == __?
+    assert Enum.min(list) == 1
   end
 
   think "getting the minimum by function" do
     list = ["the", "shortest", "word", "is", "expected"]
-    assert Enum.min_by(list, &String.length(&1)) == __?
+    assert Enum.min_by(list, &String.length(&1)) == "is"
   end
 
   think "partitioning" do
     numbers = 1..10
     {left, right} = Enum.partition(numbers, fn(x) -> rem(x, 2) == 1 end)
-    assert left == __?
-    assert right == __?
+    assert left == [1,3,5,7,9]
+    assert right == [2,4,6,8,10]
   end
 
   think "reduction" do
     numbers = 1..10
     result = Enum.reduce(numbers, 0, fn (x, acc) -> acc + x end)
-    assert result == __?
+    assert result == 55
   end
 
   think "rejection" do
     numbers = 1..10
     result = Enum.reject(numbers, fn(x) -> rem(x, 2) == 1 end)
-    assert result == __?
+    assert result == [2,4,6,8,10]
   end
 
   think "reversal" do
     numbers = 1..10
-    assert Enum.reverse(numbers) == __?
+    assert Enum.reverse(numbers) == [10,9,8,7,6,5,4,3,2,1]
   end
 
   think "shuffle" do
     numbers = 1..10
-    assert_? Enum.shuffle(numbers) == numbers
+    refute Enum.shuffle(numbers) == numbers
   end
 
   think "slicing" do
     numbers = 1..10
-    assert Enum.slice(numbers, 2, 2) == __?
+    assert Enum.slice(numbers, 2, 2) == [3,4]
   end
 
   think "slicing goes too far" do
     numbers = 1..10
-    assert Enum.slice(numbers, 2, 100) == __?
+    assert Enum.slice(numbers, 2, 100) == [3,4,5,6,7,8,9,10]
   end
 
   think "sorting" do
     numbers = [1, 6, 3, 8, 4, 2, 9, 5, 7]
-    assert Enum.sort(numbers) == __?
+    assert Enum.sort(numbers) == [1,2,3,4,5,6,7,8,9]
   end
 
   think "unique items only, please" do
     numbers = [1, 1, 2, 3, 3, 4]
-    assert Enum.uniq(numbers) == __?
+    assert Enum.uniq(numbers) == [1,2,3,4]
   end
 
   think "splitting" do
     numbers = [1, 2, 3, 4]
     {left, right} = Enum.split(numbers, 2)
-    assert left == __?
-    assert right == __?
+    assert left == [1,2]
+    assert right == [3,4]
   end
 
   think "splitting while function is true" do
     numbers = 1..10
     {left, right} = Enum.split_while(numbers, fn (x) -> x < 5 end)
-    assert left == __?
-    assert right == __?
+    assert left == [1,2,3,4]
+    assert right == [5,6,7,8,9,10]
   end
 
   think "taking some items" do
     numbers = 1..10
-    assert Enum.take(numbers, 2) == __?
+    assert Enum.take(numbers, 2) == [1,2]
   end
 
   think "taking some items the other way" do
     numbers = 1..10
-    assert Enum.take(numbers, -2) == __?
+    assert Enum.take(numbers, -2) == [9,10]
   end
 
   think "taking every so often" do
     numbers = 1..10
-    assert Enum.take_every(numbers, 3) == __?
+    assert Enum.take_every(numbers, 3) == [1,4,7,10]
   end
 
   think "taking while we can" do
     numbers = 1..10
-    assert Enum.take_while(numbers, fn (x) -> x < 5 end) == __?
+    assert Enum.take_while(numbers, fn (x) -> x < 5 end) == [1,2,3,4]
   end
 end
